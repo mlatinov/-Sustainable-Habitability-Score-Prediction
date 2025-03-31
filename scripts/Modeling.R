a4aaa0fbb06d32f4d54a80c9508049fa85c0ee86
mlatinov <latinovmetodi@gmail.com>
2025-03-31 10:47:56 +0300

    # model screening

--- scripts/Modeling.R
+++ scripts/Modeling.R
@@ -3,6 +3,8 @@
 library(tidymodels)
 library(patchwork)
 library(tidyverse)
+library(baguette)
+library(ggpubr)
 
 ## Load the data 
 training_data <- read_rds("clean_train")
@@ -75,14 +77,49 @@ null_model <- null_model() %>%
   set_engine("parsnip") %>%
   set_mode("regression")
 
+# Linear regression 
+linear_regression <- linear_reg()%>%
+  set_engine("lm") %>%
+  set_mode("regression")
+
+# MARS Untuned
+mars <- mars(
+  prod_degree = 2) %>%
+  set_engine("earth") %>%
+  set_mode("regression")
+
+# Bagged MARS
+bagged_mars <- bag_mars(
+  prod_degree = 2) %>%
+  set_engine("earth") %>%
+  set_mode("regression")
+
 # RF Untuned
 untuned_rf <- rand_forest() %>%
   set_engine("ranger") %>%
   set_mode("regression")
 
 # XGB Untuned
-untuned_xgb <- boost_tree()%>%
+untuned_xgb <- boost_tree(
+  trees = 1000 )%>%
   set_engine("xgboost") %>%
+  set_mode("regression")
+
+# Neural Network Model Untuned
+nn_model <- mlp(
+  hidden_units = 10)%>%
+  set_engine("nnet")%>%
+  set_mode("regression")
+
+# Bagged Neural Network Model Untuned
+bagged_nn_model <- bag_mlp(
+  hidden_units = 10)%>%
+  set_engine("nnet") %>%
+  set_mode("regression")
+
+# KNN Untuned
+knn_model <- nearest_neighbor(neighbors =  5 )%>%
+  set_engine("kknn") %>%
   set_mode("regression")
 
 ## Create a workflow set to compare different models with different pre-Proc and evaluate the tuning
@@ -91,14 +128,20 @@ models_workflow_set <- workflow_set(
   models = list(
     Null = null_model,
     RF = untuned_rf,
-    XGB = untuned_xgb),
+    XGB = untuned_xgb,
+    MARS = mars,
+    Bagged_mars = bagged_mars,
+    Neural_Network = nn_model,
+    Bagged_Neural_Network = bagged_nn_model,
+    Linear_Model = linear_regression,
+    KNN = knn_model),
   cross = TRUE)
 
 # Fit all the models 
 models_results <- models_workflow_set %>%
   workflow_map(
     fn = "fit_resamples",
-    resamples = vfold_cv(data = model_validation,v = 10,strata = "habitability_score"),
+    resamples = vfold_cv(data = model_validation,v = 5,strata = "habitability_score"),
     metrics = metric_set(rmse),
     verbose = TRUE,
     seed = 123)
@@ -116,19 +159,20 @@ df_plot <- df_results %>%
 # Create a Heat map plot
 plot_1 <- ggplot(data = df_plot,aes(x = preproc,y = model,fill = mean))+
   geom_tile()+
-  scale_fill_viridis_c(option = "B",direction = -1,begin = 0.1,end = 0.7)+
+  scale_fill_viridis_c(option = "D",direction = -1,begin = 0.1,end = 0.7)+
   theme_minimal()+
   labs(
     title = "Heatmap",
     y = "",
     x = " Preprocessing",
     fill = "RMSE")+
-  theme(axis.text = element_text(face = "italic"))
+  theme(
+    axis.text = element_text(face = "italic"))
 
 # Create a Bar chart
 plot_2 <- ggplot(data = df_plot,aes(x = mean,y = fct_reorder(factor(wflow_id),.fun = sum,.x = mean,.desc = TRUE),fill = model))+
   geom_col()+
-  scale_fill_viridis_d(option = "A",,begin = 0.1,end = 0.7)+
+  scale_fill_viridis_d(option = "D",,begin = 0.1,end = 0.7)+
   theme_minimal()+
   labs(
     title = "Bar Chart",
@@ -143,9 +187,8 @@ plot_2 <- ggplot(data = df_plot,aes(x = mean,y = fct_reorder(factor(wflow_id),.f
 # Create a scatter plot
 plot_3 <- ggplot(data = df_plot,aes(x = std_err, y = mean,color = model,shape = preproc)) +
   geom_point(size = 3) +
-  scale_color_viridis_d(option = "A",begin = 0.1,end = 0.7)+
+  scale_color_viridis_d(option = "D",begin = 0.1,end = 0.7)+
   theme_minimal()+
-  ylim(c(1,12))+
   labs(
     title = "Scatter Plot",
     x = "Standart Error",
@@ -157,7 +200,7 @@ plot_3 <- ggplot(data = df_plot,aes(x = std_err, y = mean,color = model,shape = 
   )
 
 # Combine all plots 
-final_plot <- (plot_2 + plot_1) / plot_3 &
+final_plot <- (plot_2 + plot_1) / plot_3  &
   plot_annotation(
     title = "Models & Preprocessing",
     subtitle = "all of the models are not tuned",
